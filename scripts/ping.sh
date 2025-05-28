5c46b068ba7cadbb6a7e261551185826aeba9669
George Hill <hillgeorge2000@gmail.com>
2025-05-28 02:14:13 +0100

    fix script

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -1,13 +1,14 @@
-  GNU nano 7.2                                                              ping.sh *                                                                      
 #!/bin/bash
 set -e
 
 IP_ADDRESS=$(curl -s -S https://ipinfo.io/ip)
+
 echo "Logging from $IP_ADDRESS"
 
 REQ_BODY=$(jq -n --arg ip "$IP_ADDRESS" --arg service "raspi-ping" '{ip_addresss: $ip, caller: $service}')
 
 set +e
-echo $(curl -s -S -X POST "http://localhost:8000/metric-instance" --data-raw "$REQ_BODY")                                                                  echo $(curl -s -S -X POST "http://192.168.178.21:8000/metric-instance" --data-raw "$REQ_BODY")
-echo $(curl -s -S -X POST "http://data.georgehill.xyz/metric-instance" --data-raw "$REQ_BODY")
-echo $(curl -s -S -X POST "http://http://135.181.89.0:8080/metric-instance" --data-raw "$REQ_BODY") 
+echo $(curl -s -S -X POST -H "Content-Type: application/json" "http://localhost:8000/metric-instance" --data-raw "$REQ_BODY")
+echo $(curl -s -S -X POST -H "Content-Type: application/json" "http://192.168.178.21:8000/metric-instance" --data-raw "$REQ_BODY")
+echo $(curl -s -S -X POST -H "Content-Type: application/json" "http://data.georgehill.xyz/metric-instance" --data-raw "$REQ_BODY")
+echo $(curl -s -S -X POST -H "Content-Type: application/json" "http://135.181.89.0:8080/metric-instance" --data-raw "$REQ_BODY")
