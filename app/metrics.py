91de2e26b0c3e8ddfa6b2880a3b012e77dd0b11e
George Hill <hillgeorge2000@gmail.com>
2025-05-29 21:12:31 +0100

    add description

--- app/metrics.py
+++ app/metrics.py
@@ -1,3 +1,3 @@
 from prometheus_client import Counter
 
-request_metric = Counter("metrictool_request", ["ip_address", "caller"])
+request_metric = Counter("metrictool_request", "requests", ["ip_address", "caller"])
